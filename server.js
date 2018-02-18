a6ac4b65641a9d9df0c295e62faf04f79c0215cc
Matt Corrigan <matthew.david.corrigan@gmail.com>
2018-02-18 09:56:01 -0600

    yeetote v2

--- server.js
+++ server.js
@@ -145,8 +145,8 @@ io.on('connection', function(socket) {
   }
 
   function turnTimer(code) {
-    setTimeout(transitionTimer.bind(null, code), 10*1000);
-    io.sockets.to(code).emit("countdown", 10);
+    setTimeout(transitionTimer.bind(null, code), 20*1000);
+    io.sockets.to(code).emit("countdown", 20);
   }
 
 });
