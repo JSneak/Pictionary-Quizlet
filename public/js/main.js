464dfd580feeffea956772cbf74c5ba6758c18dd
Matt Corrigan <matthew.david.corrigan@gmail.com>
2018-02-18 09:20:29 -0600

    yeezy ma dood

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ socket.on("start game", function(data) {
 });
 
 socket.on("update points", function(data) {
-  var names = document.getElementById("names").childNodes;
+  var names = document.getElementByClassName("names")[0].childNodes;
 
   for (var i = 0; i < names.length; i++) {
     var text = names[i].innerHTML;
